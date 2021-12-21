f011793dc1cb13d0d47efc40b5105a3c628764bf
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-12-21 11:11:37 -0800

    Improve comments

--- shell.c
+++ shell.c
@@ -12,34 +12,34 @@
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
 int main(void) {
-	char line[BLOCK] = {0};
+    char line[BLOCK] = {0};
 
-	while (true) {
-		printf("%s", PROMPT);
+    while (true) {
+        printf("%s", PROMPT);
 
-		// TODO: Currently, lines have a fixed maximum length.
-		if (fgets(line, BLOCK, stdin) == NULL) {
-			return EXIT_SUCCESS;
-		}
+        // TODO: remove line length limit.
+        if (fgets(line, BLOCK, stdin) == NULL) {
+            return EXIT_SUCCESS;
+        }
 
-		// Strip away the newline at the end of the line.
-		line[strlen(line) - 1] = '\0';
+        // Strip away the newline at the end of the line.
+        line[strlen(line) - 1] = '\0';
 
-		// Execute the program.
-		pid_t pid = fork();
-		if (pid < 0) {
-			eprintf("failed to fork\n");
-			return EXIT_FAILURE;
-		} else if (pid == 0) {
-			// TODO: Currently, no arguments are considered -- only the executable.
-			char *args[] = { line, NULL };
+        // Execute the program.
+        pid_t pid = fork();
+        if (pid < 0) {
+            eprintf("failed to fork\n");
+            return EXIT_FAILURE;
+        } else if (pid == 0) {
+            // TODO: consider all arguments.
+            char *args[] = {line, NULL};
 
-			execvp(args[0], args);
-			perror(NULL);
-		} else {
-			wait(NULL);
-		}
-	}
+            execvp(args[0], args);
+            perror(NULL);
+        } else {
+            wait(NULL);
+        }
+    }
 
     return EXIT_SUCCESS;
 }
