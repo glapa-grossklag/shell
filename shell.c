b1617a97d08330c2106f4f92cdaa708a27ee7cf7
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-12-21 11:04:01 -0800

    Add ability to execute programs

--- shell.c
+++ shell.c
@@ -1,10 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <aio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define PROMPT "$ "
 #define BLOCK 4096
+
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
 int main(void) {
 	char line[BLOCK] = {0};
@@ -12,12 +17,28 @@ int main(void) {
 	while (true) {
 		printf("%s", PROMPT);
 
+		// TODO: Currently, lines have a fixed maximum length.
 		if (fgets(line, BLOCK, stdin) == NULL) {
-			// End of file.
 			return EXIT_SUCCESS;
 		}
 
-		printf("%s", line);
+		// Strip away the newline at the end of the line.
+		line[strlen(line) - 1] = '\0';
+
+		// Execute the program.
+		pid_t pid = fork();
+		if (pid < 0) {
+			eprintf("failed to fork\n");
+			return EXIT_FAILURE;
+		} else if (pid == 0) {
+			// TODO: Currently, no arguments are considered -- only the executable.
+			char *args[] = { line, NULL };
+
+			execvp(args[0], args);
+			perror(NULL);
+		} else {
+			wait(NULL);
+		}
 	}
 
     return EXIT_SUCCESS;
