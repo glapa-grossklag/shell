395984da2befde90335ddfa28b00c36f1cc22d6b
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-12-26 14:12:37 -0800

    Format

--- shell.c
+++ shell.c
@@ -7,7 +7,6 @@
 #include <unistd.h>
 
 #define PROMPT "$ "
-#define BLOCK 4096
 #define DELIMITER " \t\r\n\a"
 
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
@@ -20,7 +19,7 @@ int main(void) {
     while (true) {
         printf("%s", PROMPT);
 
-		char *line = shell_read_line(stdin);
+        char *line = shell_read_line(stdin);
         if (line == NULL) {
             return EXIT_SUCCESS;
         }
@@ -31,10 +30,10 @@ int main(void) {
             eprintf("failed to fork\n");
             return EXIT_FAILURE;
         } else if (pid == 0) {
-			char **args = shell_split(line, DELIMITER);
+            char **args = shell_split(line, DELIMITER);
             execvp(args[0], args);
 
-			// This will only be reached if EXECVP fails above.
+            // This will only be reached if EXECVP fails above.
             perror(NULL);
         } else {
             wait(NULL);
@@ -45,60 +44,60 @@ int main(void) {
 }
 
 static char **shell_split(char *string, char *delimeter) {
-	// If a string literal is passed in, STRTOK would have a segmentation fault
-	// when it attempts to modify the string. For this reason we need a copy of
-	// the string.
-	char *copy = strdup(string);
+    // If a string literal is passed in, STRTOK would have a segmentation fault
+    // when it attempts to modify the string. For this reason we need a copy of
+    // the string.
+    char *copy = strdup(string);
 
-	// Allocate space for 32 strings to start of with, but change this size
-	// dynamically depending on what's needed.
-	size_t size = 32;
-	char **tokens = malloc(size * sizeof(char *));
+    // Allocate space for 32 strings to start of with, but change this size
+    // dynamically depending on what's needed.
+    size_t size = 32;
+    char **tokens = malloc(size * sizeof(char *));
 
-	char *token = strtok(copy, delimeter);
-	tokens[0] = token;
+    char *token = strtok(copy, delimeter);
+    tokens[0] = token;
 
-	for (size_t position = 1; ; position += 1) {
-		if (position > size) {
-			if (realloc(tokens, size * 2 * sizeof(char)) == NULL) {
-				free(tokens);
-				return NULL;
-			}
+    for (size_t position = 1;; position += 1) {
+        if (position > size) {
+            if (realloc(tokens, size * 2 * sizeof(char)) == NULL) {
+                free(tokens);
+                return NULL;
+            }
 
-			size *= 2;
-		}
+            size *= 2;
+        }
 
-		char *token = strtok(NULL, delimeter);
-		tokens[position] = token;
-		if (token == NULL) {
-			break;
-		}
-	}
+        char *token = strtok(NULL, delimeter);
+        tokens[position] = token;
+        if (token == NULL) {
+            break;
+        }
+    }
 
-	return tokens;
+    return tokens;
 }
 
 static char *shell_read_line(FILE *file) {
-	size_t size = 32;
-	char *line = malloc(size * sizeof(char));
+    size_t size = 32;
+    char *line = malloc(size * sizeof(char));
 
-	int current;
-	for (size_t position = 0; (current = fgetc(file)) != EOF; position += 1) {
-		if (position > size) {
-			if (realloc(line, size * 2 * sizeof(char)) == NULL) {
-				free(line);
-				return NULL;
-			}
+    int current;
+    for (size_t position = 0; (current = fgetc(file)) != EOF; position += 1) {
+        if (position > size) {
+            if (realloc(line, size * 2 * sizeof(char)) == NULL) {
+                free(line);
+                return NULL;
+            }
 
-			size *= 2;
-		}
+            size *= 2;
+        }
 
-		line[position] = (current == '\n' || current == EOF) ? '\0' : current;
+        line[position] = (current == '\n' || current == EOF) ? '\0' : current;
 
-		if (current == '\n' || current == EOF) {
-			return line;
-		}
-	}
+        if (current == '\n' || current == EOF) {
+            return line;
+        }
+    }
 
-	return line;
+    return line;
 }
